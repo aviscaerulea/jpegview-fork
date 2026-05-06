b83ceec5b8fb8ce1e7ce35438ae89e09e6bb0213
nikai <aviscaerulea@users.noreply.github.com>
2026-05-06 19:02:29 +0900

    Build: Setup プロジェクトを除外（WiX 未インストール対応）

--- build.bat
+++ build.bat
@@ -3,4 +3,5 @@ rem Initialize VS developer environment and run msbuild
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath`) do set VS_PATH=%%i
 call "%VS_PATH%\Common7\Tools\VsDevCmd.bat"
 if %ERRORLEVEL% NEQ 0 exit /b %ERRORLEVEL%
-msbuild %1 /p:Configuration=%2 /p:Platform=%3 /t:Rebuild /m:8 /v:minimal /nologo
+if "%MSBUILD_TARGET%"=="" set MSBUILD_TARGET=Rebuild
+msbuild %1 /p:Configuration=%2 /p:Platform=%3 /t:%MSBUILD_TARGET% /m:8 /v:minimal /nologo
